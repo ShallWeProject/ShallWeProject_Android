a30c398e1c9e6ac389bf6e83e045ba3dbfb33a59
gayeon7877 <gayean01@gmail.com>
2023-08-22 16:29:51 +0900

    Feat:API 연결

--- app/src/main/java/com/shall_we/ExperienceDetail/ExDetailFragment.kt
+++ app/src/main/java/com/shall_we/ExperienceDetail/ExDetailFragment.kt
@@ -1,60 +1,71 @@
 package com.shall_we.ExperienceDetail
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.shall_we.R
+import com.shall_we.base.BaseFragment
+import com.shall_we.databinding.FragmentExDetailBinding
+import com.shall_we.retrofit.RESPONSE_STATE
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ExDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ExDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class ExDetailFragment : BaseFragment<FragmentExDetailBinding>(R.layout.fragment_ex_detail) {
+    lateinit var experienceDetailViewModel: ExperienceDetailViewModel
+    private var experienceGiftId: Int = 1
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+    override fun init() {
+
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            experienceGiftId = it.getInt("id")
+
         }
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ex_detail, container, false)
-    }
+        experienceDetailViewModel = ViewModelProvider(this).get(ExperienceDetailViewModel::class.java)
+        experienceDetailViewModel.get_experience_detail_data(experienceGiftId, completion = {
+                responseState, responseBody ->
+            when(responseState){
+                RESPONSE_STATE.OKAY -> {
+                    Log.d("whatisthis?????", responseBody.toString())
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ExDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ExDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    responseBody?.get(0)?.let { item ->
+                        item.explanation?.forEach { explanation ->
+
+                            // Add text view
+                            val textView = TextView(requireContext())
+                            textView.text = explanation.description
+                            textView.textSize = 14f
+                            textView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+                            textView.setTextColor(Color.BLACK)
+                            textView.setPadding(0, 16, 0, 16)
+                            binding.imageContainer.addView(textView)
+
+                            // Add image view
+                            val imageView = ImageView(requireContext())
+                            Glide.with(this)
+                                .load(explanation.explanationUrl)
+                                .into(imageView)
+                            imageView.setPadding(0, 16, 0, 16)
+                            binding.imageContainer.addView(imageView)
+                        }
+                    }
+
+
+                }
+                RESPONSE_STATE.FAIL -> {
+                    Log.d("retrofit", "api 호출 에러")
                 }
             }
+        })
+
     }
+
 }
